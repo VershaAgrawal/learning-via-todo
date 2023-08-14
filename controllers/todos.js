a4af05abfa4a18769284d202dc5d67e8433225ca
VershaAgrawal <126682722+VershaAgrawal@users.noreply.github.com>
2023-08-14 21:50:16 +0530

    Added controllers (#1)

---
+++ controllers/todos.js
@@ -0,0 +1,109 @@
+const Todo = require("../models/todo");
+
+//fetch Todos
+const fetchTodos = async () => {
+  console.log("Fetching all the tasks");
+  try {
+    const todos = await Todo.find({}, { __v: 0 });
+    return {
+      statusCode: 200,
+      body: { todos },
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: { error: "Error while fetching todos: " + error.message },
+    };
+  }
+};
+
+//insert task
+const insertTask = async (inputs) => {
+  console.log("Inserting new task....");
+  try {
+    const taskText = inputs.taskText;
+
+    if (taskText == "" || typeof taskText != "string")
+      return {
+        statusCode: 400,
+        body: { error: "Invalid taskText" },
+      };
+
+    const completed = false;
+    const newTodo = new Todo({ taskText, completed });
+    await newTodo.save();
+    // const { __v, ...retTodo } = newTodo.toJSON();--- Destructing the JSON {versionKey variable, rest json variable}
+    return {
+      statusCode: 200,
+      body: { todo: newTodo.toObject({ versionKey: false }) },
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: { error: "Error while inserting new todo: " + error.message },
+    };
+  }
+};
+
+//update a task
+const updateTask = async (inputs) => {
+  console.log("Updating task details....");
+  try {
+    const { _id, completed, taskText } = inputs;
+    const myTask = await Todo.findOne({ _id: _id });
+
+    if (typeof completed != "undefined") {
+      if (typeof completed != "boolean")
+        return {
+          statusCode: 400,
+          body: { error: "Invalid completion status" },
+        };
+      myTask.completed = completed;
+    }
+
+    if (typeof taskText != "undefined") {
+      if (taskText == "" || typeof taskText != "string")
+        return {
+          statusCode: 400,
+          body: { error: "Invalid completion status" },
+        };
+      myTask.taskText = taskText;
+    }
+
+    await myTask.save();
+    return {
+      statusCode: 200,
+      body: { todo: myTask.toObject({ versionKey: false }) },
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: { error: "Error while updating task details: " + err.message },
+    };
+  }
+};
+
+//deleting a task
+const deleteTask = async (inputs) => {
+  console.log("Deleteing a task.....");
+  try {
+    const _id = inputs._id;
+    const taskToDel = await Todo.findOne({ _id: _id });
+    const delTask = await Todo.deleteOne({ _id: _id });
+    return {
+      statusCode: 200,
+      body: {
+        todo: taskToDel.toObject({ versionKey: false }),
+      },
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: {
+        error: "Error while deleting todo: " + err.message,
+      },
+    };
+  }
+};
+
+module.exports = { fetchTodos, insertTask, deleteTask, updateTask };
---
+++ controllers/validation.js
@@ -0,0 +1,12 @@
+const Todo = require("../models/todo");
+
+const checkID = async (req, res, next) => {
+  let _id = req.params._id;
+  const idRegexp = /^[0-9a-fA-F]{24}$/;
+
+  if (_id.match(idRegexp) && (await Todo.findOne({ _id: _id }))) return next();
+
+  res.status(400).json({ error: "Invalid id" });
+};
+
+module.exports = checkID;
