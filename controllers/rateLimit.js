eb4373bc6284c51bf21c83b27f9a27c1b2c6c6c7
Versha Agrawal <vershaagrawal98@gmail.com>
2023-08-20 18:54:13 +0530

    updated as per comment

--- controllers/rateLimit.js
+++ controllers/rateLimit.js
@@ -3,7 +3,14 @@ const RedisStore = require("rate-limit-redis");
 const RedisClient = require("ioredis");
 
 // Create a `ioredis` client
-const client = new RedisClient();
+const client = new RedisClient({
+  //     port: 6379, // Redis port
+  //   host: "127.0.0.1", // Redis host
+  //   username: "default", // needs Redis >= 6
+  //   password: "my-top-secret",
+  //   db: 0, // Defaults to 0
+  // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
+});
 
 const loginRateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, //1 minute
@@ -12,12 +19,6 @@ const loginRateLimiter = rateLimit({
   legacyHeaders: false,
   message: { error: "Too many requests, please try again later." },
   store: new RedisStore({
-    //     port: 6379, // Redis port
-    //   host: "127.0.0.1", // Redis host
-    //   username: "default", // needs Redis >= 6
-    //   password: "my-top-secret",
-    //   db: 0, // Defaults to 0
-    // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
     sendCommand: (...args) => client.call(...args),
   }),
 });
